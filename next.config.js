c60f91e59237be123acb89bfbeca6b253b6b9fbf
ammduncan <ammielgyanyawson@gmail.com>
2022-01-17 18:16:58 +0000

    feat: add digipress cdn to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['static.ghost.org', 'github.com']
+    domains: ['static.ghost.org', 'github.com', 'digitalpress.fra1.cdn.digitaloceanspaces.com']
   }
 }
